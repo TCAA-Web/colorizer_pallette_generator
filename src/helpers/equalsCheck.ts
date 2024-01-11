7d13a1143b04ce25306b4494c9d002ce78929e17
kasperfryd <kfry@techcollege.dk>
2024-01-11 13:41:21 +0100

    added save functionality to app

---
+++ src/helpers/equalsCheck.ts
@@ -0,0 +1,3 @@
+export const equalsCheck = (a: Array<string>, b: Array<string>) => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer/Footer";
 import { Navigation } from "../components/Navigation/Navigation";
-
+import { ToastContainer } from "react-toastify";
 export const MainLayout = () => {
   return (
     <main>
       <Navigation />
       <Outlet></Outlet>
+      <ToastContainer position="bottom-right" theme="dark" />
       <Footer />
     </main>
   );
