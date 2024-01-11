5e5e3452618f06d2fd5505886cb39b9db8200337
kasperfryd <kfry@techcollege.dk>
2024-01-11 12:34:32 +0100

    created copy to clipboard functionality

--- src/components/ColorCard/ColorCard.tsx
+++ src/components/ColorCard/ColorCard.tsx
@@ -1,14 +1,40 @@
 import style from "./colorcard.module.scss";
+import icon from "../../assets/icons8-clipboard-64.png";
+import { useState } from "react";
 
 interface ColorCardInterface {
   hexValue: string;
 }
 
 export const ColorCard = ({ hexValue }: ColorCardInterface) => {
+  const [showText, setShowText] = useState(false);
+
+  const clickHandler = () => {
+    navigator.clipboard.writeText(hexValue);
+    setShowText(true);
+    let timeout = setTimeout(() => {
+      setShowText(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  };
+
   return (
     <div className={style.cardWrapper}>
       <div style={{ backgroundColor: hexValue }} className={style.card}></div>
-      <p className={style.cardTitle}>{hexValue}</p>
+      <div>
+        <p onClick={clickHandler} className={style.cardTitle}>
+          {hexValue}
+        </p>
+        <img onClick={clickHandler} src={icon} alt="clipboard copy"></img>
+      </div>
+
+      <p
+        className={style.clipboardText}
+        style={showText ? { opacity: 1 } : { opacity: 0 }}
+      >
+        Copied to clipboard
+      </p>
     </div>
   );
 };
